2198d91b16eea1cd415d305756204ea9fd01f0d5
Gabriel Osuobiem <osuobiem@gmail.com>
2022-03-20 23:28:27 +0100

    Reconfigure app to test on Teams

--- src/pages/Configure.js
+++ src/pages/Configure.js
@@ -7,9 +7,8 @@ export default function Configure() {
 
 	msTeams.settings.registerOnSaveHandler(saveEvent => {
 		msTeams.settings.setSettings({
-			contentUrl: 'https://lively-sand-02bc7bd10.1.azurestaticapps.net',//window.location.origin,
-			entityId: "Test",
-			websiteUrl: 'https://lively-sand-02bc7bd10.1.azurestaticapps.net/configure'
+			contentUrl: window.location.origin,
+			entityId: window.location.origin
 		});
 
 	saveEvent.notifySuccess();
