086b8feeb50e20a77f44d982f6b2ce08a3569572
Gabriel Osuobiem <osuobiem@gmail.com>
2022-03-27 22:40:58 +0100

    Add theme change handler

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import { BrowserRouter } from "react-router-dom";
 import Router from "./routes/Router";
+import { Provider, teamsTheme, teamsDarkTheme, teamsHighContrastTheme  } from "@fluentui/react-northstar";
+import { useState } from "react";
+import * as msTeams from '@microsoft/teams-js';
+
+msTeams.initialize();
 
 function App() {
+    
+  const [currentTheme, setCurrentTheme] = useState('default');
+
+  const themes = {
+    default: teamsTheme, dark: teamsDarkTheme, contrast: teamsHighContrastTheme
+  };
+
+  msTeams.registerOnThemeChangeHandler(theme => {
+    setCurrentTheme(theme);
+  });
+
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <Provider theme={themes[currentTheme]}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- src/pages/HQTab.js
+++ src/pages/HQTab.js
@@ -1,80 +1,9 @@
 import React from 'react';
-import { Table, Button } from '@fluentui/react-northstar';
-import { MoreIcon } from '@fluentui/react-icons-northstar'
-import {
-  gridCellWithFocusableElementBehavior,
-} from '@fluentui/accessibility'
-
-const moreOptionCell = {
-  content: <Button tabIndex={-1} icon={<MoreIcon />} circular text iconOnly title="More options" />,
-  accessibility: gridCellWithFocusableElementBehavior,
-  onClick: e => {
-    alert('more option button clicked')
-    e.stopPropagation()
-  },
-}
-
-const header = {
-  items: [
-    {
-      key: 'flight-number',
-      content: 'Flight Number'
-    },
-    {
-      key: 'origin',
-      content: 'Origin'
-    },
-    {
-      key: 'sta',
-      content: 'STA'
-    },
-    {
-      key: 'std',
-      content: 'STD'
-    },
-    {
-      key: 'status',
-      content: 'Status'
-    },
-    {
-      key: 'destination',
-      content: 'Destination'
-    },
-    {
-      key: 'performance',
-      content: 'Perfomance'
-    },
-    {
-      key: 'time-on-ground',
-      content: 'Time on Ground'
-    },
-    {
-      key: 'more options',
-      'aria-label': 'options',
-    }
-  ]
-};
-
-const rows = [
-  {
-    key: 1,
-    items: [
-      { key: '1-1', content: 'EA243' },
-      { key: '1-2', content: 'Abuja' },
-      { key: '1-3', content: '10:00' },
-      { key: '1-4', content: '12:00' },
-      { key: '1-5', content: 'Departed' },
-      { key: '1-6', content: 'Lagos' },
-      { key: '1-7', content: 'In - Time' },
-      { key: '1-8', content: '01:35' },
-      { key: '1-9', ...moreOptionCell }
-    ]
-  },
-];
+import HQTable from '../helpers/Table/HQTable';
 
 const HQTab = () => {
   return (
-    <Table header={header} rows={rows}/>
+    <HQTable/>
   );
 };
 
