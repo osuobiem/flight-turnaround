75e150681ade685e93f1808ce37509883546bc6b
Gabriel Osuobiem <osuobiem@gmail.com>
2022-04-14 13:41:45 +0100

    People picker experiment

--- src/pages/AdminDashboard.js
+++ src/pages/AdminDashboard.js
@@ -13,9 +13,9 @@ const AdminDashboard = () => {
     const accessToken = async () => {
       dispatchAuthEvent(await serverReq('getToken'));
     }
-    
+
     accessToken();
-  }, []);
+  }, [dispatchAuthEvent]);
 
   return (
     <div>
