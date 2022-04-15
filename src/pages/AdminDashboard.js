deb3805a4891f68a263dcbbdd5a42611e44add76
Gabriel Osuobiem <osuobiem@gmail.com>
2022-04-16 00:52:52 +0100

    Popup window experiment

--- src/pages/AdminDashboard.js
+++ src/pages/AdminDashboard.js
@@ -8,27 +8,20 @@ msTeams.initialize();
 
 const AdminDashboard = () => {
 
-  const popWindow = () => {
-    let authTokenRequest = {
-      successCallback: function (result) {
-        console.log(result);
-        alert(result);
-        //call server side to exchange the  token from teams with access token & used it to call graph
-        // fetch("/token?token=" + result)              
-        //             .then(json => json.json()).then(result=> {
-        //                 document.getElementById("jsonData").textContent = JSON.stringify(result, undefined, 2);
-        //             });
-
-
-      },
-      failureCallback: function (error) { console.log("Failure: " + error); alert("Failure: " + error); },
-    };
-    msTeams.authentication.getAuthToken(authTokenRequest);
-  };
+  const popWindow = async () => {
+    await msTeams.authentication.authenticate({
+      url: window.location.origin + "/consent-popup-start.html",
+      width: 600,
+      height: 535,
+      successCallback: (() => {
+        console.log('Got success callback');
+      })
+    });
+  }
 
   return (
     <div>
-      <button onClick={popWindow()}>Click Me</button>
+      <button onClick={() => popWindow()}>Click Me</button>
       <AdminHeader />
       <ManageTeams />
     </div>
